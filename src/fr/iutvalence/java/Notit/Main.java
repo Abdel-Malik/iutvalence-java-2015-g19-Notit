f9ae11b23ffc0136844262c307d7f1f2cf6cc150
anthony <anthony.lafont@outlook.com>
2015-06-06 15:45:16 +0200

    test editNote

--- src/fr/iutvalence/java/Notit/Main.java
+++ src/fr/iutvalence/java/Notit/Main.java
@@ -15,17 +15,26 @@ public class Main
 {
 
 	public static void main(String[] args) throws IOException
-	{
-		
+	{	
 		MainFrame intermarche = new MainFrame();
 		// 0: January, 11:December 
 		Calendar calendar = new Calendar(5, 2015);
 		System.out.println(calendar);
-			
-		Set<GeneralNote> listGeneralNote = new NoteList().getlistOfGeneralNote();
+		for(Date date : calendar.getListOfDateForAMonth()){
+			if (date.getListOfNote().size() != 0){
+				for(Note note : date.getListOfNote()){
+					System.out.println(note);
+				}
+			}
+		}
+		
+		/*Set<GeneralNote> listGeneralNote = new NoteList().getlistOfGeneralNote();
 		for(GeneralNote note : listGeneralNote){
 			System.out.println(note);
-		}
+		}*/
+		
+		DayNote maNote = new DayNote(2, "coucou", "essai modif editNote", new Date(2015, 5, 26));
+		maNote.EditNote();
 	}
 
 }
