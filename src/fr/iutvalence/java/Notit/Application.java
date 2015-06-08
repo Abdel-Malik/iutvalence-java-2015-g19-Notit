e13e67b6ea2cfe72141d42b037daac48e177a37d
Abdel-Malik <bouhassoun.abdel-malik@iut-valence.fr>
2015-06-08 12:31:13 +0200

    method Application

--- src/fr/iutvalence/java/Notit/Application.java
+++ src/fr/iutvalence/java/Notit/Application.java
@@ -7,7 +7,7 @@ package fr.iutvalence.java.Notit;
 public class Application {
 	
 	
-	public void editDayNotes(int number, String title, String content, Date date){
+	/*public void editDayNotes(int number, String title, String content, Date date){
 		DayNote theNote = new DayNote(number, title, content, date);
 		theNote.editNote();
 			theNote.setTitle(title);
@@ -17,6 +17,6 @@ public class Application {
 	public void editDayNotes(String title, String content, Date date){
 		String path = "Notes/"+theNote.getDate().getDate();
 		editNotes(Path.maxFileInPath(path), title, content, date);
-	}
+	}*/
 
 }
