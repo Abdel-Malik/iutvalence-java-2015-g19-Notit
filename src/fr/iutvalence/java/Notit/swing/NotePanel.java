dad59a3a1aef796a83dad97e1f724a93593f04c8
gurbuza <adem.gurbuz@iut-valence.fr>
2015-06-09 14:20:09 +0200

    JOptionPanel for delete or not the post

--- src/fr/iutvalence/java/Notit/swing/NotePanel.java
+++ src/fr/iutvalence/java/Notit/swing/NotePanel.java
@@ -5,16 +5,19 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+
 import fr.iutvalence.java.Notit.Application;
 import fr.iutvalence.java.Notit.DayNote;
 import fr.iutvalence.java.Notit.GeneralNote;
 
 public class NotePanel extends JPanel implements ActionListener{
-	
+
 	private JLabel noteName;
 	private JLabel descriptionNote;
 	private JButton deleteNote;
@@ -26,31 +29,31 @@ public class NotePanel extends JPanel implements ActionListener{
 	private Application application;
 	private MainFrame theFrame;
 	private DayPanel panel;
-	
+
 	public NotePanel(GeneralNote generalNote, MainFrame theFrame){
 		this.generalNote=generalNote;
 		display(generalNote.getTitle(), generalNote.getContent(), theFrame);
 	}
-	
+
 	public NotePanel(DayNote dayNote, MainFrame theFrame){
 		this.dayNote = dayNote;
 		display(dayNote.getTitle(), dayNote.getContent(), theFrame);
 	}
-	
+
 	public NotePanel(DayNote dayNote, DayPanel panel, MainFrame theFrame){
 		this.panel = panel;
 		this.dayNote = dayNote;
 		display(dayNote.getTitle(), dayNote.getContent(), theFrame);
 	}
-		
+
 	public void display(String name, String content, MainFrame theFrame){
 		this.theFrame = theFrame;
 		this.application = theFrame.getApplication();
 		Dimension dimForButton = new Dimension(70, 30);
 		Dimension dimForLabel = new Dimension(150,30);
-		
+
 		this.setPreferredSize(new Dimension(250, 75));
-		
+
 		/**
 		 * JLabel
 		 */
@@ -58,7 +61,7 @@ public class NotePanel extends JPanel implements ActionListener{
 		this.noteName.setPreferredSize(dimForLabel);
 		this.descriptionNote = new JLabel(content);
 		this.descriptionNote.setPreferredSize(dimForLabel);
-		
+
 		/**
 		 * JButton
 		 */
@@ -66,7 +69,7 @@ public class NotePanel extends JPanel implements ActionListener{
 		this.deleteNote.setPreferredSize(dimForButton);
 		this.editNote = new JButton("edit");
 		this.editNote.setPreferredSize(dimForButton);
-		
+
 		this.deleteNote.addActionListener(this);
 		this.editNote.addActionListener(this);
 		/**
@@ -74,61 +77,69 @@ public class NotePanel extends JPanel implements ActionListener{
 		 */
 		this.buttonSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, deleteNote, editNote);
 		this.labelSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, noteName, descriptionNote);	
-		
+
 		this.buttonSplit.setDividerSize(0);
 		this.labelSplit.setDividerSize(0);
-		
+
 		this.add(buttonSplit, BorderLayout.WEST);
 		this.add(labelSplit, BorderLayout.EAST);
-		
-		
+
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		if(e.getSource()==this.deleteNote){
+		if(e.getSource()==this.deleteNote){     
 			
-			if( this.generalNote == null){
-				try {
-					this.application.deleteDayNotes(this.dayNote);
-				} catch (IOException e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}	
-				
-				if(this.panel==null)
-				{
+			int jop = JOptionPane.showConfirmDialog(
+				    this.theFrame,
+				    "Would you delete this Not'it?",
+				    "Delete",
+				    JOptionPane.YES_NO_OPTION);
+			
+			if(jop==JOptionPane.YES_OPTION){
+				if( this.generalNote == null){
 					try {
-						this.theFrame.getHomePage().displayDayNote();
-					} catch (IOException e1) {
+						this.application.deleteDayNotes(this.dayNote);
+					} catch (IOException e2) {
 						// TODO Auto-generated catch block
-						e1.printStackTrace();
+						e2.printStackTrace();
+					}	
+
+					if(this.panel==null)
+					{
+						try {
+							this.theFrame.getHomePage().displayDayNote();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					}
+					else{
+						try {
+							this.panel.displayDayNotePanel();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+
 				}
 				else{
 					try {
-						this.panel.displayDayNotePanel();
+						this.application.deleteGNotes(this.generalNote);
+						this.theFrame.getHomePage().displayGeneralNote();
 					} catch (IOException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-				}
-
-			}
-			else{
-				try {
-					this.application.deleteGNotes(this.generalNote);
-					this.theFrame.getHomePage().displayGeneralNote();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
 				}
 			}
 
 
 		}
-		
+
 		if(e.getSource()==this.editNote){
 			if( this.generalNote == null){
 				if(this.panel==null)
